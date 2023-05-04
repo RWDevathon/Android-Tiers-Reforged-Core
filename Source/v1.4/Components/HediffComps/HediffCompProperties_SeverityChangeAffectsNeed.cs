361a7f515516ca8742f60bae01c0e40f15faae5f
Killathon <huelva21@gmail.com>
2023-05-04 16:58:46 -0700

    (MH) ATR Core Coolant experimental version

---
+++ Source/v1.4/Components/HediffComps/HediffCompProperties_SeverityChangeAffectsNeed.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using Verse;
+
+namespace ATReforged
+{
+    public class HediffCompProperties_SeverityChangeAffectsNeed : HediffCompProperties
+    {
+        public NeedDef needDef;
+
+        public float severityToChangeRatio = 1f;
+
+        public bool changeOnIncrease = true;
+
+        public HediffCompProperties_SeverityChangeAffectsNeed()
+        {
+            compClass = typeof(HediffComp_SeverityChangeAffectsNeed);
+        }
+    }
+}
