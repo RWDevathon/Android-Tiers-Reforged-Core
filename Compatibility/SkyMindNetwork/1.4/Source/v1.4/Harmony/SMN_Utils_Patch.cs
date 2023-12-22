c6dedbcf490b4825bab85b3dfb33091875fc99bb
Killathon <huelva21@gmail.com>
2023-12-22 12:52:12 -0500

    Androids actually lose interface on SkyMind download

---
+++ Compatibility/SkyMindNetwork/1.4/Source/v1.4/Harmony/SMN_Utils_Patch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using SkyMind;
+using Verse;
+
+namespace ATReforged
+{
+    public class SMN_Utils_Patch
+    {
+        // Androids that are turned into blanks via SkyMind connection ought to have the no installed interface hediff.
+        [HarmonyPatch(typeof(SMN_Utils), "TurnIntoBlank")]
+        public class TurnIntoBlank_Patch
+        {
+            [HarmonyPostfix]
+            public static void Listener(Pawn pawn, bool shouldRemoveSkyMindImplants)
+            {
+                if (ATRCore_Utils.IsConsideredMechanicalAndroid(pawn) && shouldRemoveSkyMindImplants)
+                {
+                    pawn.health.AddHediff(ATR_HediffDefOf.ATR_IsolatedCore, pawn.health.hediffSet.GetBrain());
+                }
+            }
+        }
+    }
+}
